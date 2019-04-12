a2f2b1a0f01a0e01ee94beed20e1329f539d5723
Unsar Malik <unsar.malik24@gmail.com>
2019-04-12 01:38:34 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -12,6 +12,10 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-contacts
-end
+contacts.each do |person, info|
+    info.each do |attribute, value|
+      if attribute==:favorite_icecream_flavors
+        value.delete_if {|flavor| flavor=="strawberry"}
+      end 
+    end
 
